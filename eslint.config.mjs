807f9836eda6050e9e8f5ac9204674bd6a298729
antonio-victor <antoniovictormt@gmail.com>
2025-10-28 11:34:55 -0300

    feat: config eslint and prettier alongside with CI/CD

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,44 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { FlatCompat } from "@eslint/eslintrc"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+import { dirname } from "path"
+import { fileURLToPath } from "url"
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const __filename = fileURLToPath(import.meta.url)
 
-export default eslintConfig;
+const __dirname = dirname(__filename)
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname
+})
+
+const eslintConfig = [
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        plugins: { "simple-import-sort": simpleImportSort },
+        rules: {
+            "simple-import-sort/imports": "error",
+            "simple-import-sort/exports": "error",
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    vars: "all",
+                    args: "after-used",
+                    ignoreRestSiblings: true,
+                    varsIgnorePattern: "^_"
+                }
+            ],
+            "no-console": "error",
+            semi: ["error", "never"],
+            quotes: ["error", "double"],
+            "padding-line-between-statements": [
+                "error",
+                { blankLine: "always", prev: "import", next: "*" },
+                { blankLine: "always", prev: "export", next: "*" },
+                { blankLine: "always", prev: "const", next: "export" },
+                { blankLine: "any", prev: "import", next: "import" }
+            ]
+        }
+    }
+]
+
+export default eslintConfig
